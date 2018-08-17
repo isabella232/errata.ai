0af5e0bd6ffd38f8d069f160a3ae1b93f2a646ed
Joseph Kato <joseph@jdkato.io>
2018-08-16 20:28:09 -0700

    [WIP] Landing page

--- static/js/typewrite.js
+++ static/js/typewrite.js
@@ -16,3 +16,29 @@ typewriter.typeString('static site generators.')
     .typeString('automated testing.')
     .pauseFor(2500)
     .start();
+
+
+$(document).ready(function () {
+    $("#myCarousel").on("slide.bs.carousel", function (e) {
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 3;
+        var totalItems = $(".carousel-item").length;
+
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+            var it = itemsPerSlide - (totalItems - idx);
+            for (var i = 0; i < it; i++) {
+                // append slides to end
+                if (e.direction == "left") {
+                    $(".carousel-item")
+                        .eq(i)
+                        .appendTo(".carousel-inner");
+                } else {
+                    $(".carousel-item")
+                        .eq(0)
+                        .appendTo($(this).find(".carousel-inner"));
+                }
+            }
+        }
+    });
+});
