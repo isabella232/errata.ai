a0cce2b835d7ece7b57b55fbabe4ed6de2b07cf1
Joseph Kato <joseph@jdkato.io>
2021-07-08 16:18:17 -0700

    Fix date formatting in non-Chrome browsers

--- static/js/manage.js
+++ static/js/manage.js
@@ -1,46 +1,49 @@
-const API = "https://vale-studio.herokuapp.com/api/user/";
-var KEY = null;
+const API = 'https://vale-studio.herokuapp.com/api/user/'
+let KEY = null
 
 $(document).ready(function () {
-    function handleError(e) {
-        $("#activations, #back-to-key").fadeOut();
-        $("#license-section, #forgot-key").fadeIn(300);
-        alert("No activations were found for this license key.");
+  function handleError (e) {
+    $('#activations, #back-to-key').fadeOut()
+    $('#license-section, #forgot-key').fadeIn(300)
+    alert('No activations were found for this license key.')
 
-        KEY = null;
-        console.log(e);
-    }
+    KEY = null
+    console.log(e)
+  }
 
-    $("#license-section").submit(function (e) {
-        e.preventDefault();
-        $("#license-section, #forgot-key").fadeOut(300);
+  $('#license-section').submit(function (e) {
+    e.preventDefault()
+    $('#license-section, #forgot-key').fadeOut(300)
 
-        KEY = $("#license-input").val().trim();
-        $("#key-caption").html("license: <code>" + KEY + "</code>");
+    KEY = $('#license-input').val().trim()
+    $('#key-caption').html('license: <code>' + KEY + '</code>')
 
-        $.ajax(API + "activations?key=" + KEY)
-            .done(function (ret) {
-                result = JSON.parse(ret);
-                if (result === null || result.length === 0) {
-                    handleError("no activations found");
-                } else {
-                    $("#act-list tbody").empty();
-                    $("#activations, #back-to-key").delay(300).fadeIn();
-                    $(".activations-list").html("");
+    $.ajax(API + 'activations?key=' + KEY)
+      .done(function (ret) {
+        result = JSON.parse(ret)
+        if (result === null || result.length === 0) {
+          handleError('no activations found')
+        } else {
+          $('#act-list tbody').empty()
+          $('#activations, #back-to-key').delay(300).fadeIn()
+          $('.activations-list').html('')
 
-                    for (var i = 0; i < result.length; i++) {
-                        var curr = result[i];
-                        var date = new Date(curr.Date);
-                        var name = curr.Name.toLowerCase();
+          for (let i = 0; i < result.length; i++) {
+            const curr = result[i]
 
-                        var icon = "fa-linux";
-                        if (name.includes("win")) {
-                            icon = "fa-windows";
-                        } else if (name.includes("mac")) {
-                            icon = "fa-apple";
-                        }
+            const dstamp = curr.Date.replace(/-/g, '/').split('.')[0]
 
-                        var activationHTML =
+            const date = new Date(dstamp)
+            const name = curr.Name.toLowerCase()
+
+            let icon = 'fa-linux'
+            if (name.includes('win')) {
+              icon = 'fa-windows'
+            } else if (name.includes('mac')) {
+              icon = 'fa-apple'
+            }
+
+            const activationHTML =
                             '<tr class="text-small">' +
                             '<th scope="row">' +
                             '<span" class="d-flex align-items-center">' +
@@ -48,52 +51,52 @@ $(document).ready(function () {
                             '<i class="fab ' +
                             icon +
                             ' desktop-icon"></i>' +
-                            "</div>" +
+                            '</div>' +
                             '<span class="text-body">' +
                             curr.Name +
-                            "</span>" +
-                            "</span>" +
-                            "</th>" +
-                            "<td>" +
+                            '</span>' +
+                            '</span>' +
+                            '</th>' +
+                            '<td>' +
                             date +
-                            "</td>" +
-                            "<td>Individual</td>" +
-                            "<td>" +
+                            '</td>' +
+                            '<td>Individual</td>' +
+                            '<td>' +
                             '<button type="button" class="btn btn-sm btn-danger button-deactivate" data-mach="' +
                             curr.Mach +
                             '">Deactivate</button>' +
-                            "</td>" +
-                            "</tr>";
+                            '</td>' +
+                            '</tr>'
 
-                        $("#act-list tbody").append(activationHTML);
-                    }
-                }
-            })
-            .fail(handleError);
-    });
+            $('#act-list tbody').append(activationHTML)
+          }
+        }
+      })
+      .fail(handleError)
+  })
 
-    $(".back").click(function () {
-        $("#activations, #back-to-key").fadeOut(300);
-        $("#license-section, #forgot-key").delay(300).fadeIn();
-        $("#license-input").focus();
-    });
+  $('.back').click(function () {
+    $('#activations, #back-to-key').fadeOut(300)
+    $('#license-section, #forgot-key').delay(300).fadeIn()
+    $('#license-input').focus()
+  })
 
-    $("body").on("click", ".button-deactivate", function (e) {
-        const stamp = $(this).data("mach");
-        const entry = $(this).closest("tr");
-        $.ajax({
-            url: API + "deactivate",
-            type: "POST",
-            data: { license: KEY, machine: stamp },
-        })
-            .done(function (result) {
-                entry.remove();
-            })
-            .fail(function (e) {
-                console.log(e);
-                alert(
-                    "Connection error. Sorry for the inconvenience, please try again later."
-                );
-            });
-    });
-});
+  $('body').on('click', '.button-deactivate', function (e) {
+    const stamp = $(this).data('mach')
+    const entry = $(this).closest('tr')
+    $.ajax({
+      url: API + 'deactivate',
+      type: 'POST',
+      data: { license: KEY, machine: stamp }
+    })
+      .done(function (result) {
+        entry.remove()
+      })
+      .fail(function (e) {
+        console.log(e)
+        alert(
+          'Connection error. Sorry for the inconvenience, please try again later.'
+        )
+      })
+  })
+})
